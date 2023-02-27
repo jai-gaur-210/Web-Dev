4af17b924d6fdee3ab7715cdb43682b8c353d59b
jai <jaigaur.imp210@gmail.com>
2023-02-27 18:43:31 +0530

    commit

---
+++ JS/demo.js
@@ -0,0 +1,7 @@
+let namasteBtn =document.querySelector('button');
+namasteBtn.addEventListener('click' , inputMsg);
+
+function inputMsg(){
+    let name = prompt('Enter name of Student');
+    namasteBtn.textContent = 'Roll no. 1: ' + name;
+}
