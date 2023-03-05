311450e85bd4e79f3771a59921ef5ea58d3b0289
jai <jaigaur.imp210@gmail.com>
2023-03-05 16:30:08 +0530

    commit

--- Calculator/cal.js
+++ Calculator/cal.js
@@ -9,24 +9,4 @@ function sol() {
     let y = eval(x)
     document.getElementById("text").value = y
 }
-function del() {
-    let display = document.getElementById("text");
-    let currentValue = display.value;
-    if (currentValue.length === 1) {
-        display.value = "0";
-    } else {
-        display.value = currentValue.slice(0, -1);
-    }
-}
-function toggleMinus() {
-    const input = document.getElementById("text");
-    let value = input.value;
 
-    if (value.charAt(0) === "-") {
-        value = value.substring(1);
-    } else {
-        value = "-" + value;
-    }
-
-    input.value = value;
-}
